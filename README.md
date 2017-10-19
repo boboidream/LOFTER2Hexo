f10c8d2124e33297f2dd5036f8ab410d343c0087
hancong <hancong@zhihu.com>
2017-10-19 11:37:17 +0800

    ..

---
+++ README.md
@@ -0,0 +1,70 @@
+# LOFTER2Hexo
+一个将 LOFTER 导出的 XML 文件转换为适合 Hexo 解析的 Markdown 文件工具。
+
+![效果图](http://7xsmek.com1.z0.glb.clouddn.com/2017-04-16.gif)
+
+项目地址: https://github.com/boboidream/LOFTER2Hexo
+
+## 使用说明
+
+### 环境配置
+1. node.js : 如需安装请查看 [Node.js 下载](https://nodejs.org/en/download/)
+
+2. 安装项目
+
+    ```
+    sudo npm install lofter2hexo -g
+    ```
+
+    > 如果国内速度太慢，可以设置淘宝 npm 源，命令行执行  
+    > `npm config set registry https://registry.npm.taobao.org`
+
+### 使用程序
+
+1. 将 LOFTER 导出的 XML 文件命名为: `LOFTER.xml`
+
+2. 打开终端，`cd` 至 `LOFTER.xml` 所在文件夹，运行 `readxml`
+
+### 运行结果
+
+1. 在 `Terminal` 中会打印日志
+
+2. 生成 `LOFTER` 文件夹，包含所有 Markdown 文件；所有图片下载到 `LOFTER/img` 文件夹下
+
+### 参数说明
+```
+  Usage: readxml [options]
+
+  Options:
+
+    -h, --help           output usage information
+    -V, --version        output the version number
+    -i, --input <lang>   xml 文件路径，例如：`/Volumes/私人/Github/test.xml`
+    -n, --notag          头部不生成 tags 标签（以避免生成太多 `Tags` 造成的不美观）
+    -j, --jekyll         导出 jekyll 格式 Markdown 文件
+    -a, --author <lang>  设置 jekyll 格式 Markdown 头部 author
+```
+
+命令举例
+```bash
+readxml -i ./Github/test.xml -n 
+# 解析路径为 `./Github/test.xml` 文件，生成 Markdown 文件不带 Tags 标签
+
+
+readxml -n -j -a boboidream 
+# 解析当前目录下 LOFTER.xml 文件，生成不带 Tags 标签的 Jekyll 格式 Markdown 文件，头部 author: boboidream
+```
+
+## 其他说明
+【已修复】有些朋友留言说，导出 Markdown 文件为空的问题，本人未测试出问题。希望有问题的朋友能提供下运行环境和终端日志，以便尽快修复问题，感谢！
+
+
+```
+版本说明:
+2017.04.16 做成 node 命令行工具，发布到 npm
+2017.03.20 支持下载图片到本地 `./LOFTER/img` 文件夹，更改生成 md 中图片路径。
+2016.07.31 感谢 「屠夫9441」反馈bug，修复名称相同文件覆盖问题。
+2016.06.28 感谢 「Teng」反馈 bug，修复 音乐文件引起的 Okb 问题，并支持导出 jekyll 所用md格式
+2016.06.26 配置了 package.json文件，并将依赖文件直接放置于项目中，简化使用步骤
+2016.02.26 发布
+```
---
+++ bin/readxml.js
@@ -0,0 +1,222 @@
+#! /usr/bin/env node
+
+/*
+ * Author: boboidream
+ * Version: 2.0.3
+ * Update: 20170415
+ */
+
+var fs = require('fs'),
+    xml2js = require('/usr/local/lib/node_modules/xml2js'),
+    toMarkdown = require('/usr/local/lib/node_modules/to-markdown'),
+    parser = new xml2js.Parser(),
+    image_downloader = require('/usr/local/lib/node_modules/image-downloader'),
+    argv = require('/usr/local/lib/node_modules/commander')
+
+// init commander
+argv.version('2.0.3')
+    .usage('[options]')
+    .option('-i, --input <lang>', 'lofter xml file')
+    .option('-n, --notag', 'Without tags')
+    .option('-j, --jekyll', 'jekyll type')
+    .option('-a, --author <lang>', 'set author in header in jekyll type')
+    .parse(process.argv)
+
+// init path
+var path = require('path'),
+    cwd = process.cwd(),
+    outputFilePath = path.resolve(cwd, 'LOFTER'),
+    file = argv.input || path.resolve(cwd, 'LOFTER.xml'),
+    author = argv.author || ''
+
+// main object
+var lofter2hexo = {
+    run: function() {
+        var lib = this.lib
+
+        lib.getPostArray(file, function(postArray) {
+            lib.parsePost(postArray , lib.parseArticle, lib.createMD)
+        })
+    },
+    lib: {
+        getPostArray: function(file, callback) {
+            fs.readFile(file, function(err, data) {
+
+                if(err) {
+                    console.error(`读取文件 ${file} 出错：`, err)
+                    return
+                }
+
+                parser.parseString(data, function(error, result) {
+                    callback(result.lofterBlogExport.PostItem)
+                })
+            })
+        },
+        parsePost: function(postArray, parseArticle, createMD) {
+            postArray.forEach(function(article, index) {
+                var newDate = new Date(parseInt(article.publishTime)).Format("yyyy-MM-dd hh:mm:ss"),
+                    fileName = newDate.substring(0, 10) + '-' + (article.title || '无题') + '.md',
+                    allWord = parseArticle(article)
+
+                if (fileName.indexOf('/') != null) {
+                    var fileName = fileName.replace(/\//, ' ');
+                }
+
+                createMD(fileName, allWord, index);
+            })
+        },
+        createMD: function(fileName, allWord, i) {
+            fs.open(path.resolve(outputFilePath, fileName), 'w', function(err) {
+                if (err) throw err;
+
+                fs.writeFile(path.resolve(outputFilePath, fileName), allWord, function(err) {
+                    if (err) throw err;
+                    console.log(i + '. Create ' + fileName + ' successfully!');
+                })
+            })
+        },
+        parseArticle: parsearticle
+    }
+}
+
+
+// parse one article
+function parsearticle(article) {
+    var _parparseHeader,
+        _parseContent,
+        _parseComment,
+        _downloadImg,
+        headline = '',
+        content = '',
+        comments = '',
+        newDate = new Date(parseInt(article.publishTime)).Format("yyyy-MM-dd hh:mm:ss")
+
+    _parseHeader = function() {
+        var tags = argv.notag ? '' : article.tag,
+            newDate = newDate = new Date(parseInt(article.publishTime)).Format("yyyy-MM-dd hh:mm:ss")
+
+            if (argv.jekyll) {
+                var res = ''
+                if (tags) {
+                    tags.forEach(function(tag) {
+                        res += '    - ' + tag + '\n';
+                    })
+                }
+
+                headline =  '---\n' +
+                            'layout: post\n' +
+                            'title: "' + (article.title || '无题') + '"\n' +
+                            'date: ' + newDate + '\n' +
+                            'author: "' + author + '"\n' +
+                            'catalog: 随笔' + '\n' +
+                            'tags: \n' + res + '\n---\n'
+            } else {
+
+                headline = '---\n' + 'title: ' + (article.title || '无题') + '\n' +
+                        'date: ' + newDate + '\n' +
+                        'categories: 随笔' + '\n' +
+                        'tags: [' + tags + ']\n\n---\n'
+            }
+
+            return headline
+    }
+
+    _parseContent = function() {
+        if (article.content) {
+            var imgArray = []
+
+            content = toMarkdown(article.content.toString())
+            imgArray = content.match(/!\[.*?\]\((.*?)\)/g)
+
+            if (imgArray && imgArray.length) {
+                imgArray.forEach(function(imgURL) {
+                    imgURL = imgURL.match(/http.*\.jpg|http.*\.jpeg|http.*\.gif|http.*\.png/)
+                    if (imgURL) {
+                      content = content.replace(/!\[(.*?)\]\((.*?)\)/g, function(whole, imgName, url) {
+                          _downloadImg(imgURL[0], imgName, (article.title || '无题'))
+                          return `![${imgName}](${imgURL[0].split('/').pop()})`
+                      })
+                    }
+                })
+            }
+        } else if (article.photoLinks != null) {
+            var text = article.photoLinks[0],
+                imgArray = JSON.parse(text)
+
+            imgArray.forEach(function(img) {
+                var imgName = img.small.split('/').pop(),
+                    imgURL = img.small
+
+                content += '![图片]' + '(' + imgName + ')\n'
+                _downloadImg(imgURL, imgName, (article.title || '无题'))
+            })
+
+        } else if (article.caption != null) {
+
+            content = toMarkdown(article.caption.toString());
+
+        } else {
+            console.log(article);
+        }
+
+        return content
+    }
+
+    _parseComment = function() {
+        if (article.commentList == null) {
+            return comments
+        }
+        var comment = article.commentList[0].comment
+
+        for (var i = 0; i < comment.length; ++i) {
+            var newDate = new Date(parseInt(comment[i].publishTime)).Format('yyyy-MM-dd hh:mm:ss'),
+                item = '**' + comment[i].publisherNick + '：** ' + comment[i].content + '  *[' + newDate + ']*\n>\n';
+
+            comments += item;
+        }
+
+        return '\n---\n' + '>评论区：\n>' + comments;
+    }
+
+    _downloadImg = function(imgURL, imgName, title) {
+              var imagePath = path.resolve(cwd, 'LOFTER/' + newDate.substring(0, 10) + '-' + title)
+              if (!fs.existsSync(imagePath)) {
+                  fs.mkdirSync(imagePath, 0755)
+              }
+             image_downloader({
+                url: imgURL,
+                dest: imagePath,
+                done: function(err, imgName, image) {
+                    if (err) {
+                        console.log(err)
+                    }
+                    console.log('Image saved to ', imgName)
+                }
+            })
+        }
+
+    return _parseHeader() + _parseContent() + _parseComment()
+}
+
+
+// Date format author: meizz
+Date.prototype.Format = function(fmt) {
+    var o = {
+        "M+": this.getMonth() + 1, //月份
+        "d+": this.getDate(), //日
+        "h+": this.getHours(), //小时
+        "m+": this.getMinutes(), //分
+        "s+": this.getSeconds(), //秒
+        "q+": Math.floor((this.getMonth() + 3) / 3), //季度
+        "S": this.getMilliseconds() //毫秒
+    };
+    if (/(y+)/.test(fmt))
+        fmt = fmt.replace(RegExp.$1, (this.getFullYear() + "").substr(4 - RegExp.$1.length));
+    for (var k in o)
+        if (new RegExp("(" + k + ")").test(fmt))
+            fmt = fmt.replace(RegExp.$1, (RegExp.$1.length == 1) ? (o[k]) : (("00" + o[k]).substr(("" + o[k]).length)));
+    return fmt;
+}
+
+
+lofter2hexo.run()
